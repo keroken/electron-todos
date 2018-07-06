a8a8d21a9eec21beeda71eebceae9568f7743cf6
Kenichi Shinagawa <kenichi.shinagawa@gmail.com>
2018-07-07 02:03:30 +0900

    added basic menu

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({  });
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { label: 'New Todo' }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Toggle Developer  Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
